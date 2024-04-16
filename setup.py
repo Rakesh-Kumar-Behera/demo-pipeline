edf1df0fed74e6dbf8352f4a08978c2b5a8c1e88
behera.r.k2015@gmail.com <behera.r.k2015@gmail.com>
2024-04-17 01:19:02 +0530

    all env-config files added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+setup(name = "Census-Income",
+      version = "0.0.1",
+      author = "Rakesh",
+      author_email = "rakeshkumarbehera342@gmail.com",
+      find_packages = find_packages(),
+      install_requires =["pandas","numpy","flask"]
+      )
+
+
